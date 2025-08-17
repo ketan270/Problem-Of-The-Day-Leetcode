3623668d905855f9c99088b977516406dda52c0a
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2025-08-17 20:14:30 +0530

    Create 118. Pascal's Triangle.java

---
+++ 118. Pascal's Triangle.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for(int i = 0; i < numRows; i++){
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for(int j = 1;j < i;j++){
+                row.add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
+            }
+            if (i > 0) row.add(1);
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
